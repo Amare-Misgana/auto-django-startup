1e5f3903c2979b3304ba3c1a88ce9426ca4c8326
Amare Misgana <codeamare@gmail.com>
2025-04-17 10:03:20 +0300

    Update corrected.py

--- corrected.py
+++ corrected.py
@@ -25,7 +25,7 @@ class AutoDjango:
     {% block content %}{% endblock %}  
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    {% block script %}{% endblock %}
+    <script>{% block script %}{% endblock %}</script>
 </body>  
 </html>  
 """
